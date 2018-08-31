dfbed6337091ccd78dff75dbca9905e084eb3a93
evan <e.b.e.markel@astro.uio.no>
2018-08-31 11:53:02 -0400

    fixed errors

--- src/prediction-validation.py
+++ src/prediction-validation.py
@@ -4,26 +4,26 @@ import numpy as np
 import pandas as pd
 
 
-def main(window_size, actual_price, predicted_price, output_error):
+def main(window, actual_price, predicted_price, output_error):
     #read in window size from input file
-    window_size = with open(window_size_file, 'r')
-                  as file: window_size = int(file.read())
+    window_size = None
+    with open(window, 'r') as file: window_size = int(file.read())
 
     #read in actual and predicted price files as dataframes
     actual_df = pd.read_csv(actual_price, delimiter='|', header=None, index_col=['hour','ID'],
-                            names=['hour','ID','price'], skip_blank_lines=True, float_precistion='high')
+                            names=['hour','ID','price'], skip_blank_lines=True, float_precision='high')
     predicted_df = pd.read_csv(predicted_price, delimiter='|', header=None, index_col=['hour','ID'],
-                            names=['hour','ID','price'], skip_blank_lines=True, float_precistion='high')
+                            names=['hour','ID','price'], skip_blank_lines=True, float_precision='high')
 
     #join datasets and calculate rolling average error between model and actual prices
     combined_df = actual_df.join(predicted_df, how='left', lsuffix='_actual',rsuffix='_predicted')
     combined_df['abs_error'] =  np.abs(combined_df['price_actual'] - combined_df['price_predicted'])
-    combined_df['rolling_error'] = combined_df['abs_error'].rolling(window_size, min_periods=1).avg()
+    combined_df['rolling_error'] = combined_df['abs_error'].rolling(window_size, min_periods=1).mean()
 
     #create output data and save to to csv
     output_df = combined_df[['hour','avg_err']].set_index('hour')
     output_df['end_hour'] = output_df['hour'] + window_size -1
-    output_df[['time', 'end_time', 'avg_error']].to_csv(output_file,delimiter='|',header=None,na_rep='NA',float_format='%.2f')
+    output_df[['hour', 'end_hour', 'avg_error']].to_csv(output_file,delimiter='|', header=None, na_rep='NA', float_format='%.2f')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
